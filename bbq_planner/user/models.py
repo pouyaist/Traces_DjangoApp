560202ddb8b0fb77b1a04492562d2707cee7a6bd
Pouya Samadi <pspouya90@gmail.com>
2018-02-26 19:42:59 +0100

    create a minimal User table

--- bbq_planner/user/models.py
+++ bbq_planner/user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class UserProfile(models.Model):
+    user_auth = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20, default = None, blank = True)
+
+    class Meta:
+        db_table = 'user_profiles'
