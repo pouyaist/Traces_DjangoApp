29296ddb04712972beeee4dc320ac4f01486643b
Pouya Samadikhah <pspouya90@gmail.com>
2018-03-07 15:55:23 +0100

    css footer

--- bbq_planner/bbq_planner/settings.py
+++ bbq_planner/bbq_planner/settings.py
@@ -52,7 +52,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
